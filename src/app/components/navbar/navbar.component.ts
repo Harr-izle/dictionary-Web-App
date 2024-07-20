a79eba537d7cca31e112e211e956fa201dfa392b
Harriet Buadee <117269443+Harr-izle@users.noreply.github.com>
2024-07-20 00:30:29 -0700

    implements font switching

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule,],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   darkTheme = false;
+  isDropdownVisible = false;
+  selectedFont = 'Sans Serif';
+
+// themeSwitching
   toggleTheme() {
     console.log("hey");
     this.darkTheme = !this.darkTheme;
@@ -20,6 +25,30 @@ export class NavbarComponent {
     }
   }
 
- 
+  // fontSwitching
+  
+
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
+  }
+  
+  changeFont(font: string) {
+    this.selectedFont = font;
+    this.isDropdownVisible = false;
+  
+    switch (font) {
+      case 'Sans Serif':
+        document.body.style.fontFamily = 'var(--font-primary)';
+        break;
+      case 'Serif':
+        document.body.style.fontFamily = 'var(--font-secondary)';
+        break;
+      case 'Mono':
+        document.body.style.fontFamily = 'var(--font-tertiary)';
+        break;
+      default:
+        document.body.style.fontFamily = '';
+    }
+  }
 
 }
